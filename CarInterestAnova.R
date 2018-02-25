04ee3ceb6c19c93a4d503e7ed782ba7b90811af4
Beatriz Rivera <rivers.tris@gmail.com>
2018-02-25 11:42:06 +0800

    initial commit

---
+++ CarInterestAnova.R
@@ -0,0 +1,8 @@
+r <- c(t(as.matrix(carInterestRate))) #response data
+f <- c("Atlanta", "Chicago", "Houston", "Memphis", "New York", "Philadelphia") #treatment levels
+k <- 6 #number of treatment levels
+n <- 9 #observations per treatment
+tm <- gl(k, 1, n*k, factor(f)) #matching treatments
+av <- aov(r ~ tm)
+
+summary(aov)
