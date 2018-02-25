b79e8238d53623f805788e88ac6d5f16d7f56926
Beatriz Rivera <rivers.tris@gmail.com>
2018-02-25 14:05:56 +0800

    updated

--- SmartPhoneRatingAnova.R
+++ SmartPhoneRatingAnova.R
@@ -1,15 +1,15 @@
-r <- c(t(as.matrix(SmartPhoneRating))) #response data
+r.smartphone <- c(t(as.matrix(SmartPhoneRating))) #response data
 
 f1 <- c("AT&T", "Sprint", "T-Mobile", "Verizon") #1st factor levels
 f2 <- c(">= 150", "< 150") #2nd factor levels
 k1 <- length(f1)  
 k2 <- length(f2)
-n <- 3 #observations per treatment
+n.sp <- 3 #observations per treatment
 
-tm1 <- gl(k1, 1, n*k1*k2, factor(f1))
+tm1 <- gl(k1, 1, n.sp*k1*k2, factor(f1))
 
 tm2 <- gl(k2, n*k1, n*k1*k2, factor(f2))
 
-av.smartphone <- aov(r ~ tm1 * tm2) #include interaction
+av.smartphone <- aov(r.smartphone ~ tm1 * tm2) #include interaction
 
 summary(av.smartphone)
